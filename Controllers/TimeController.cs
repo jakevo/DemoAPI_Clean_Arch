25576e4c5ffcb04901127dc33b8fad53035a37ee
jake vo <jakevoett@outlook.com>
2021-08-03 00:20:27 -0700

    add api project

---
+++ Controllers/TimeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeController
+    {
+        private readonly ILogger<TimeController> _logger;
+
+        public TimeController(ILogger<TimeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public DateTime Get() => DateTime.Now;
+    }
+}
